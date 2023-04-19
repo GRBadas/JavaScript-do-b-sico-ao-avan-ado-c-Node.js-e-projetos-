859570f2930a1f3825c094cb307922497cb183dc
Gabriel Badaró <grbadas@gmail.com>
2023-04-19 18:20:11 -0300

    fim da seção 5

--- Exercícios/exer01.js
+++ Exercícios/exer01.js
@@ -1,4 +1,4 @@
-const nome = "Badas"
+/* const nome = "Badas"
 let idade = 24
 const serVivo = true
 
@@ -10,11 +10,13 @@ if (idade >= 18){
         console.log("Cresce e aparece")
 }
 
+
 if (nome == 'Badas') {
     console.log("É o Badas :o")
 } else {
     console.log("Você não é o Badas >:(")
 }
+
 
 function calcPow(base, expo) {
     console.log(Math.pow(base, expo));
@@ -23,3 +25,37 @@ let num1 = 2; let num2 = 3; let num3 = 18
 calcPow(num1,2)
 calcPow(num2,2)
 calcPow(num3,2)
+
+let vel = 90
+if(vel > 80) {
+    console.log("Multado, você está acima de 80km/h" + ` \n Multa por dirigir a ${vel}km/h `)
+} else {
+
+    console.log("Tá safe, você está abaixo de 80km/h")
+} 
+
+
+let cnh = false
+if(idade >= 18 && cnh == false) {
+    console.log("Você precisa tirar sua CNH para dirigir !")
+} else if ( idade >= 18 && cnh == true) {
+    console.log("Vrum vrum . . . .")
+} else console.log("Você não tem idade para dirigir, nem habilitação.")
+
+for (let i = 0; i < 11; i++) {
+    console.log(i)
+}
+
+for (let i = 100; i >= 50; i--) {
+    console.log(i)
+}
+
+for (let i = 0; i <= 50; i++) {
+    if (i%2) {
+        console.log(`Ímpar: ` + i)
+    } else {
+        console.log('Par: ' + i)
+    }
+}
+*/ 
+
