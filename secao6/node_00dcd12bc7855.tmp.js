0810ff729c3da3dda8278aa03f065845b8bf6150
Gabriel Badaró <grbadas@gmail.com>
2023-04-21 23:43:08 -0300

    Closure

---
+++ secao6/node_00dcd12bc7855.tmp.js
@@ -0,0 +1,38 @@
+/* function imprimirNoConsole() {
+    console.log("Imprimiu :o")
+}
+
+imprimirNoConsole()
+
+function imprimirUmNumero(num) {
+    console.log(`O número é ${num}`)
+}
+
+imprimirUmNumero(3)
+
+const funcao = (a, b) => {
+    return a + b;
+}
+
+console.log(funcao(3,4))
+
+const d20 = () => {
+    console.log(Math.random())
+}
+
+d20()
+
+const dobrar = (n) => n*2
+
+console.log(dobrar(6)) */
+
+function subtrair(x) {
+    return function(y) {
+        return x - y; 
+    }
+}
+
+let nx = subtrair(5)
+console.log(nx(3))
+
+let ny = subtrair
