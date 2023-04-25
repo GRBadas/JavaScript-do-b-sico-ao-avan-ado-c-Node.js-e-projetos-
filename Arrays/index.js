fa20094f3a8fb8605e73f388fa22683a386ff78e
Gabriel Badaró <grbadas@gmail.com>
2023-04-25 18:47:45 -0300

    Arrays

---
+++ Arrays/index.js
@@ -0,0 +1,11 @@
+let numeros = [1,5,48,52,23,485,2]
+numeros.push(99)
+
+for(let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i])
+}
+
+elementoRemovido = numeros.pop()
+
+
+console.log(elementoRemovido)
